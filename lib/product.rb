dd4cd46abad194097572a3918835ed5429aea79a
mayday77-77 <mayday77@gmail.com>
2016-03-23 23:16:16 +0800

    Done with Duplicate Title check coding

--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,35 @@
 class Product
 
+	attr_reader :title, :price, :stock
+
 	@@products = []
 
 	def initialize(options = {})
-		@@products << self
+		@title = options[:title]
+		add_to_product
 	end
 
 	def self.all
 		@@products
 	end
 
+private
+
+	def add_to_product
+		duplicate = 0
+		@@products.each do | each_product |
+			if each_product.title == @title
+				duplicate = 1
+				break
+			end
+		end
+
+		if duplicate == 0
+			@@products << self
+		else
+			raise DuplicateProductError, "#{@title} already exists"
+		end
+
+	end
 
 end
