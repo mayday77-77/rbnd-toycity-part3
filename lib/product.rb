204686abeed4da81419df761b5cda742eab52dbe
mayday77-77 <mayday77@gmail.com>
2016-03-24 15:37:10 +0800

    Completed up products_in_stock section of app.rb

--- lib/product.rb
+++ lib/product.rb
@@ -6,30 +6,57 @@ class Product
 
 	def initialize(options = {})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_product
+	end
+
+	def in_stock?
+		# need to reverse the boolean because if zero? is true, it means we have no stock, thus the need to return false
+		!@stock.zero?
 	end
 
 	def self.all
 		@@products
 	end
 
-private
-
-	def add_to_product
-		duplicate = 0
+	# loop through product array and return the object if found
+	# Revisit : maybe need to take care if not found?
+	# Revisit : maybe try to refine the search, it seems repeated
+	def self.find_by_title(input_title)
 		@@products.each do | each_product |
-			if each_product.title == @title
-				duplicate = 1
-				break
+			if each_product.title == input_title
+				return each_product
 			end
 		end
+	end
 
-		if duplicate == 0
-			@@products << self
+	def self.in_stock
+		in_stock_array = @@products.select {| each_product | each_product.in_stock?}
+	end
+
+#private_class_method :all
+
+private
+
+	# Adding to product array depending on the title check
+	def add_to_product
+		if product_title_exists?(@title)
+			begin
+				raise DuplicateProductError
+			rescue Exception => e
+				puts e.message + ": '#{@title}' already exists"
+			end
 		else
-			raise DuplicateProductError, "#{@title} already exists"
+			@@products << self
 		end
+	end
 
+	# Method to check if title exists in the products array and return boolean
+	def product_title_exists?(input_title)
+		@@products.each do | each_product |
+			return true if each_product.title == input_title; end
+		return false					
 	end
 
 end
