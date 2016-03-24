d4f576315c33b04fd50942850b19a08214306ef0
mayday77-77 <mayday77@gmail.com>
2016-03-24 16:18:08 +0800

    Completed Customer.rb

---
+++ lib/customer.rb
@@ -0,0 +1,46 @@
+class Customer
+
+	attr_accessor :name
+
+	@@customers = []
+
+	def initialize(options = {})
+		@name = options[:name]
+		add_to_customers
+	end
+
+	def self.all
+		@@customers
+	end
+
+	def self.find_by_name(input_name)
+		@@customers.each do | each_customer |
+			if each_customer.name == input_name
+				return each_customer
+			end
+		end
+	end
+
+private
+
+	# Adding to customer array depending on the name check
+	def add_to_customers
+		if cust_name_exists?(@name)
+			begin
+				raise DuplicateCustomerError
+			rescue Exception => e
+				puts e.message + ": '#{@name}' already exists"
+			end
+		else
+			@@customers << self
+		end
+	end
+
+	# Method to check if name exists in the customers array and return boolean
+	def cust_name_exists?(input_name)
+		@@customers.each do | each_customer |
+			return true if each_customer.name == input_name; end
+		return false					
+	end
+
+end
